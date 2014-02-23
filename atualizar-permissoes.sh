b8dc7e71f627854c71a80bce21fe3be35c5c0d6c
Alisson Chiquitto <chiquitto@gmail.com>
2014-02-23 09:58:15 -0300

    atualizar-permissoes.sh

--- atualizar-permissoes.sh
+++ atualizar-permissoes.sh
@@ -1,2 +1,2 @@
-sudo chown -R unipar:unipar /home/unipar/Trabalho/MaterialAulas2014
-sudo chmod -R -0777 /home/unipar/Trabalho/MaterialAulas2014
+chown -R unipar:unipar /home/unipar/Trabalho/MaterialAulas2014
+chmod -R 777 /home/unipar/Trabalho/MaterialAulas2014
