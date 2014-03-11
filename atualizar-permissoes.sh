9aad6c9f4b4cdc705817662cb27cf4096959c017
Alisson Chiquitto <chiquitto@gmail.com>
2014-03-11 16:34:29 -0300

    * atualizar-permissoes

--- atualizar-permissoes.sh
+++ atualizar-permissoes.sh
@@ -1,10 +1,17 @@
 # ref: http://stackoverflow.com/questions/3740152/how-to-set-chmod-for-a-folder-and-all-of-its-subfolders-and-files-in-linux-ubunt
 
 # Set owner of files
-chown -R alisson:alisson /home/alisson/Trabalho/MaterialAulas2014
+chown -R unipar:unipar /home/unipar/Trabalho/MaterialAulas2014
 
 # Set chmod 777 for directories
-find /home/alisson/Trabalho/MaterialAulas2014 -type d -exec chmod 777 {} \;
+find /home/unipar/Trabalho/MaterialAulas2014 -type d -exec chmod 777 {} \;
 
 # Set chmod 666 for files
-find /home/alisson/Trabalho/MaterialAulas2014 -type f -exec chmod 666 {} \;
+find /home/unipar/Trabalho/MaterialAulas2014 -type f -exec chmod 666 {} \;
+
+# ==
+
+rm -f /home/alisson/Trabalho/MaterialAulas2014
+
+# Criar link simbolico
+ln -s /home/unipar/Trabalho/MaterialAulas2014/ /home/alisson/Trabalho/MaterialAulas2014
