adbb8c6f1cf310ee41626ab0b5fee22d59e177a2
Alisson Chiquitto <chiquitto@gmail.com>
2014-03-11 15:49:55 -0300

    * gitignore

--- atualizar-permissoes.sh
+++ atualizar-permissoes.sh
@@ -1,10 +1,10 @@
 # ref: http://stackoverflow.com/questions/3740152/how-to-set-chmod-for-a-folder-and-all-of-its-subfolders-and-files-in-linux-ubunt
 
 # Set owner of files
-chown -R unipar:unipar /home/unipar/Trabalho/MaterialAulas2014
+chown -R alisson:alisson /home/alisson/Trabalho/MaterialAulas2014
 
 # Set chmod 777 for directories
-find /home/unipar/Trabalho/MaterialAulas2014 -type d -exec chmod 777 {} \;
+find /home/alisson/Trabalho/MaterialAulas2014 -type d -exec chmod 777 {} \;
 
 # Set chmod 666 for files
-find /home/unipar/Trabalho/MaterialAulas2014 -type f -exec chmod 666 {} \;
+find /home/alisson/Trabalho/MaterialAulas2014 -type f -exec chmod 666 {} \;
