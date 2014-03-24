0ce4d18f821e20cc12de261e82dfa2d89dce28c1
Alisson Chiquitto <chiquitto@gmail.com>
2014-03-24 20:18:46 -0300

    * PHP Design Patterns - DAO DO

---
+++ unipar-cianorte/tads/servicos-internet/design-patterns-php/scripts/mvc/lib/Conexao.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Classe de Conexao
+ *
+ * @author alisson
+ */
+class Conexao extends PDO {
+
+    private $dsn = 'mysql:host=localhost;dbname=sistema_dao';
+    private $user = 'root';
+    private $password = '123456';
+    private static $instancia;
+
+    function __construct() {
+        try {
+            parent::__construct($this->dsn, $this->user, $this->password);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Conexão falhou. Erro: " . $e->getMessage();
+            exit;
+        }
+    }
+
+    /**
+     * 
+     * @return Conexao
+     */
+    public static function getInstance() {
+        if (null === self::$instancia) {
+            self::$instancia = new self();
+        }
+        return self::$instancia;
+    }
+
+}
