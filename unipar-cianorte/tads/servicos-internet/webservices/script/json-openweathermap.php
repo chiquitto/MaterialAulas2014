13379b9cf80cb4f0b1dc8705d1562aedcdcf8a38
Alisson Chiquitto <chiquitto@gmail.com>
2014-05-06 20:29:29 -0300

    * Webservice PHP & JSON

--- unipar-cianorte/tads/servicos-internet/webservices/script/json-openweathermap.php
+++ unipar-cianorte/tads/servicos-internet/webservices/script/json-openweathermap.php
@@ -10,7 +10,7 @@ $url = "";
 
 // Carregar arquivo com o JSON
 
-// Decodificar JSON
+// Decodificar JSON para stdClass
 
 ?>
 <!DOCTYPE html>
@@ -19,19 +19,8 @@ $url = "";
         <title>Tempo na cidade</title>
     </head>
     <body>
-        <h1>Tempo no momento na cidade: Cianorte</h1>
+        <h1>Tempo no momento na cidade: Nome da cidade</h1>
         
-        <p>Temperatura atual: 19.184°C</p>
-        <p>Direção do vento: 76.0021graus</p>
-        <p>Condição: IMG light rain</p>
-        <p>Pressão: xxx hPa</p>
-        <p>Intensidade dos ventos: xx km/h</p>
-        <p>Humidade: xx %</p>
-        
-        <p>Latidude: 99</p>
-        <p>Longitude: 99</p>
-        
-        <p>Atualizado em: 06/05/2014 19:37:59hrs</p>
         
     </body>
 </html>
