8bad7e4d7beb21da7e48bc16ef4c9b7f3392d32f
Alisson Chiquitto <chiquitto@gmail.com>
2014-05-15 20:15:16 -0300

    PHP POST

--- unipar-cianorte/tads/aplicacoes-web/php-get-post/scripts/form2.php
+++ unipar-cianorte/tads/aplicacoes-web/php-get-post/scripts/form2.php
@@ -1,3 +1,27 @@
 <?php
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
-print_r($_POST);
+if (isset($_POST['logado'])) {
+	$logado = 'Permanecer logado';
+}
+else {
+	$logado = 'Não permanecer logado';
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tela de informações</title>
+  <meta charset="utf-8">
+</head>
+<body>
+
+	<h1>Informações informadas</h1>
+
+	<p>Email: <?php echo $email; ?></p>
+	<p>Senha: <?php echo $senha; ?></p>
+	<p>Permanecer logado: <?php echo $logado; ?></p>
+
+</body>
+</html>
---
+++ unipar-cianorte/tads/aplicacoes-web/php-get-post/scripts/form3.php
@@ -0,0 +1,59 @@
+<?php
+$msg = '';
+$email = '';
+$logado = false;
+
+if ($_POST) {
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+
+	if (isset($_POST['logado'])) {
+		$logado = true;
+	}
+
+	if ($email == '') {
+		$msg .= 'Informe um email.';
+	}
+	if ($senha == '') {
+		$msg .= 'Informe uma senha.';
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tela de login</title>
+  <meta charset="utf-8">
+</head>
+<body>
+
+	<h1>Tela de login</h1>
+
+	<?php if ($msg != '') { ?>
+	<p><?php echo $msg; ?></p>
+	<?php } ?>
+
+	<form action="form3.php" method="post" name="frm1">
+		<fieldset>
+			<legend>Informações para login</legend>
+			<p>
+				Email:
+				<input type="email" name="email" value="<?php echo $email; ?>">
+			</p>
+			<p>
+				Senha:
+				<input type="password" name="senha">
+			</p>
+			<p>
+				Manter-se logado:
+				<input type="checkbox" name="logado" value="1"<?php if($logado){ ?> checked<?php } ?>>
+			</p>
+			<p>
+				<input type="submit" name="entrar" value="Entrar">
+			</p>
+		</fieldset>
+	</form>
+
+</body>
+</html>
+
