8835de28c7cbb0f8b925ea476bc3d222ced8957a
Alisson Chiquitto <chiquitto@gmail.com>
2014-05-27 18:32:52 -0300

    PHP - Soap Correios

---
+++ unipar-cianorte/tads/servicos-internet/webservices/soap-correios/frete.php
@@ -0,0 +1,17 @@
+<?php
+// Informacoes para serem enviadas
+$std = new stdClass;
+$std->nCdEmpresa = '';
+$std->sDsSenha = '';
+$std->nCdServico = '40010,41106';
+$std->sCepOrigem = '87200-163';
+$std->sCepDestino = '21941-916';
+$std->nVlPeso = '3,5';
+$std->nCdFormato = 1;
+$std->nVlComprimento = 30;
+$std->nVlAltura = 10;
+$std->nVlLargura = 15;
+$std->nVlDiametro = 0;
+$std->sCdMaoPropria = 'N';
+$std->nVlValorDeclarado = '0';
+$std->sCdAvisoRecebimento = 'N';
