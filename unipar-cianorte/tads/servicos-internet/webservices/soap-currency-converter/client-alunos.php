839b2734e6cd225d3b21943f631c2d5f4a43ca3d
Alisson Chiquitto <chiquitto@gmail.com>
2014-05-28 17:10:10 -0300

    PHP Soap Currency Converter

---
+++ unipar-cianorte/tads/servicos-internet/webservices/soap-currency-converter/client-alunos.php
@@ -0,0 +1,15 @@
+<?php
+$uri = 'http://www.webservicex.net/CurrencyConvertor.asmx?WSDL';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Conversão de moedas</title>
+  <meta charset="utf-8">
+</head>
+<body>
+	<!-- Exemplo -->
+	<!-- <p>1 EUR = X BRL</p> -->
+</body>
+</html>
+
