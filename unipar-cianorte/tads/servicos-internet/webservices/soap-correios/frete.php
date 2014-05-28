78374f59597f380e51f54269169537ce4a86b6c3
Alisson Chiquitto <chiquitto@gmail.com>
2014-05-28 14:53:49 -0300

    PHP - Soap Correios

--- unipar-cianorte/tads/servicos-internet/webservices/soap-correios/frete.php
+++ unipar-cianorte/tads/servicos-internet/webservices/soap-correios/frete.php
@@ -15,3 +15,22 @@ $std->nVlDiametro = 0;
 $std->sCdMaoPropria = 'N';
 $std->nVlValorDeclarado = '0';
 $std->sCdAvisoRecebimento = 'N';
+
+// Objeto SOAP
+$uri = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?WSDL';
+$soap = new SoapClient($uri);
+
+// Resposta do Webservice
+$resposta = $soap->CalcPrecoPrazo($std);
+
+$servicos = $resposta->CalcPrecoPrazoResult->Servicos;
+?>
+<h1>Frete de <?php echo $std->sCepOrigem; ?>
+    para <?php echo $std->sCepDestino; ?></h1>
+
+<p>Valor: R$
+    <?php echo $servicos->cServico[0]->Valor; ?></p>
+
+<p>Prazo de entrega:
+    <?php echo $servicos->cServico[0]->PrazoEntrega; ?>
+    dias uteis</p>
