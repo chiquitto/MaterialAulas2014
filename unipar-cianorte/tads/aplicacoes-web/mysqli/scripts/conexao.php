780df24d1135ad490cea8f72a4ad020e63963a56
Alisson Chiquitto <chiquitto@gmail.com>
2014-05-28 14:53:35 -0300

    PHP - mysqli_*

--- unipar-cianorte/tads/aplicacoes-web/mysqli/scripts/conexao.php
+++ unipar-cianorte/tads/aplicacoes-web/mysqli/scripts/conexao.php
@@ -1,3 +1,20 @@
 <?php
 
-$con = mysqli_connect('localhost','root','123456','aula');
+$con = mysqli_connect('localhost', 'root', '123456', 'aula');
+
+//var_dump($con);
+
+//mysqli_close($con);
+
+$sql = 'Select * From cliente';
+
+$registros = mysqli_query($con, $sql);
+
+if (!$registros) {
+	echo mysqli_error($con);
+	exit;
+}
+
+while($linha = mysqli_fetch_assoc($registros)) {
+	echo $linha['nome'], '<br>';
+}
