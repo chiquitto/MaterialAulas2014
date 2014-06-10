3e4a8b3809ebd34d7e63d60582026b24ab26e440
Alisson Chiquitto <chiquitto@gmail.com>
2014-06-10 20:33:53 -0300

    PHP - Webservices

--- unipar-cianorte/tads/servicos-internet/webservices/funcoes-basicas-webservices.php
+++ unipar-cianorte/tads/servicos-internet/webservices/funcoes-basicas-webservices.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
-  Carrega um arquivo, seja local ou via protocolos como HTTP e FTP
-
-  @name file_get_contents
-  @link http://www.php.net/function.file-get-contents
-*/
+ * Carrega um arquivo, seja local ou via protocolos como HTTP e FTP
+ *
+ * @name file_get_contents
+ * @link http://www.php.net/function.file-get-contents
+ */
 
 // Carregar um arquivo local
-$a = file_get_contents('/home/unipar/arquivo.txt');
+$a = file_get_contents('/home/diretorio/arquivo.txt');
 
 // Carregar um arquivo via HTTP
-$a = file_get_contents('http://unipar.br');
+$a = file_get_contents('http://site.com.br');
 
 // Carregar arquivos via HTTP usando método POST
 // http://stackoverflow.com/questions/2445276/how-to-post-data-in-php-using-file-get-contents
