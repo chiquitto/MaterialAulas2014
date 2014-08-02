7afb78497a51e79505449489dda5ae97c8d09ade
Alisson Chiquitto <chiquitto@gmail.com>
2014-08-02 09:20:05 -0300

    2serie - Cadastrar/Editar categoria

--- unipar-cianorte/tads/aplicacoes-web/sistema-venda/categorias-editar.php
+++ unipar-cianorte/tads/aplicacoes-web/sistema-venda/categorias-editar.php
@@ -5,44 +5,9 @@ require './lib/funcoes.php';
 require './lib/conexao.php';
 
 $msg = array();
-$categoria = '';
-$ativo = 1;
 
 if ($_POST) {
-  // Pegar informações
-  $categoria = $_POST['categoria'];
 
-  if(isset($_POST['ativo'])){
-    $ativo = 1;
-  } else {
-    $ativo = 0;
-  }
-    
-  // Validar informações
-  if ($categoria == ''){
-    $msg[] = 'Informe a categoria';
-  }
-    
-  // Inserir
-  if (!$msg){
-    $sql = "insert into categoria
-    values (null, '$categoria', '$ativo')";
-    
-    $resultado = mysqli_query($con, $sql);
-
-    // Testar se foi inserido
-    if (!$resultado) {
-      $msg[] = 'Nao foi possivel inserir o registro.';
-      $msg[] = mysqli_error($con); 
-    } else {
-      $idcategoria = mysqli_insert_id($con);
-      $url = 'categorias-editar.php?idcategoria=' . $idcategoria;
-      $mensagem = 'Categoria cadastrada!';  
-
-      javascriptAlertFim($mensagem, $url);
-    }
-
-  }
 }
 
 ?>
@@ -67,7 +32,7 @@ if ($_POST) {
 
 <?php if ($msg) { msgHtml($msg); } ?>
 
-<form role="form" method="post" action="categorias-cadastrar.php">
+<form role="form" method="post" action="categorias-editar.php">
     
   <div class="form-group">
     <label for="fcategoria">Categoria</label>
