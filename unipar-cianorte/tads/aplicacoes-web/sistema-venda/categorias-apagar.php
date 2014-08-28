ab301deb4a0856b7843bff5efe4e8e1947ded06a
Alisson Chiquitto <chiquitto@gmail.com>
2014-08-28 20:39:43 -0300

    2serie - Apagar registro

---
+++ unipar-cianorte/tads/aplicacoes-web/sistema-venda/categorias-apagar.php
@@ -0,0 +1,34 @@
+<?php
+    require './config.php';
+    require './lib/conexao.php';
+    require './lib/funcoes.php';
+
+if(isset($_GET['idcategoria'])){
+    $idcategoria = (int) $_GET['idcategoria'];
+}
+else{
+    echo 'Categoria não encontrada';
+    exit;
+}
+
+$sql = "select count(*) as quantidade from produto where idcategoria = $idcategoria";
+$consulta = mysqli_query($con, $sql);
+$resultado = mysqli_fetch_assoc($consulta);
+if($resultado['quantidade'] >0){
+    $msg = 'Não é possivel eliminar esta categoria pois existem produtos relacionados a ela';
+}
+else{
+    $sql = "delete from categoria where idcategoria = $idcategoria";
+
+    $deletar = mysqli_query($con, $sql);
+
+    if(mysqli_affected_rows($con)==1){
+        $msg = "Registro $idcategoria eliminado";
+    }
+    else{
+        $msg = 'Registro não encontrado';
+    }
+}
+$url = 'categorias.php';
+javaScriptAlertFim($msg, $url);
+?>
---
+++ unipar-cianorte/tads/aplicacoes-web/sistema-venda/clientes-apagar.php
@@ -0,0 +1,29 @@
+<?php
+    
+    require './config.php';
+    require './lib/conexao.php';
+    require './lib/funcoes.php';
+
+if(isset($_GET['idcliente'])){
+    $idcliente= (int) $_GET['idcliente'];
+}
+else{
+    echo 'Código invalido';
+    exit;
+}
+$sql = "delete from cliente where idcliente = $idcliente";
+$deletar = mysqli_query($con, $sql);
+
+if(mysqli_affected_rows($con)==1){
+    $msg = "Registro $idcliente eliminado" ;
+}
+else{
+    $msg = 'Registro não encontrado';
+}
+$url = 'clientes.php';
+
+javaScriptAlertFim($msg, $url);
+?>
+
+
+
