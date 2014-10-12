3b89d632e2c81910d8f056a8073b01cf7877ca2a
Alisson Chiquitto <chiquitto@gmail.com>
2014-10-12 10:01:14 -0300

    1serie - JS - Formularios

---
+++ unipar-cianorte/tads/interfaces/javascript/form/form.js
@@ -0,0 +1,15 @@
+var nomeUsuario;
+
+var pegarNome = function() {
+  nomeUsuario = window.prompt('Qual seu nome?');
+}
+
+var mostrarNome = function(){
+  var inputNome = document.getElementById('fnome');
+  inputNome.value = nomeUsuario;
+}
+
+pegarNome();
+
+// Isto não irá funcionar
+mostrarNome();
