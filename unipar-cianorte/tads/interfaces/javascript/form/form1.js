135a2d8caaaec13c591c689e275831a67bc773ac
Alisson Chiquitto <chiquitto@gmail.com>
2014-10-21 14:57:04 -0200

    1serie - JS - Formularios

--- unipar-cianorte/tads/interfaces/javascript/form/form1.js
+++ unipar-cianorte/tads/interfaces/javascript/form/form1.js
@@ -1,15 +1,49 @@
 var nomeUsuario;
+var sobrenomeUsuario;
 
 var pegarNome = function () {
   nomeUsuario = window.prompt('Qual seu nome?');
+  sobrenomeUsuario = window.prompt('Qual seu sobrenome?');
 }
 
 var mostrarNome = function(){
   var inputNome = document.getElementById('fnome');
+  
   inputNome.value = nomeUsuario;
+  inputNome.disabled = true;
+  
+  var inputNome2 = document.getElementById('fnome2');
+  inputNome2.value = sobrenomeUsuario;
 }
 
 pegarNome();
 
-// Isto não irá funcionar
-mostrarNome();
+//window.alert('Carregando...');
+window.onload = function() {
+  //window.alert('HTML carregado');
+  
+  mostrarNome();
+  
+  var f = document.getElementById('fform');
+  f.onsubmit = salvar;
+}
+
+function salvar(evento) {
+  var email1 = document.getElementById('femail');
+  var email2 = document.getElementById('femail2');
+  
+  if (email1.value != email2.value) {
+    email1.style.backgroundColor = '#FF0000';
+    email2.style.backgroundColor = 'rgb(255,0,0)';
+    
+    window.alert('Os 2 emails devem ser iguais.');
+    evento.preventDefault();
+    return false;
+  }
+  
+}
+
+
+
+
+
