748f05ba56d081235f8a41e55770634a0fad0bbb
Alisson Chiquitto <chiquitto@gmail.com>
2014-11-01 09:39:59 -0200

    1serie - UrnaEletronicaHTML5 - Vereador OK

--- unipar-cianorte/tads/interfaces/UrnaEletronicaHTML5/lib/js/urna.js
+++ unipar-cianorte/tads/interfaces/UrnaEletronicaHTML5/lib/js/urna.js
@@ -19,7 +19,7 @@ candidatos['vereador'] = [];
 
 candidatos['prefeito'][0] = ['88', 'Paper Mario', 'PSM'];
 candidatos['prefeito'][1] = ['99', 'Sonic', 'PSA'];
-candidatos['prefeito'][2] = ['77', 'Pikachu', 'PSM'];
+candidatos['prefeito'][2] = ['77', 'Pikachu', 'PMB'];
 
 candidatos['vereador'][0] = ['88001', 'Goomba', 'PSM'];
 candidatos['vereador'][1] = ['88014', '????', 'PSM'];
@@ -28,15 +28,15 @@ candidatos['vereador'][3] = ['88456', 'Cogumelo', 'PSM'];
 candidatos['vereador'][4] = ['88666', 'Boo', 'PSM'];
 candidatos['vereador'][5] = ['88999', 'Florzinha', 'PSM'];
 
-candidatos['vereador'][5] = ['99555', 'Tails', 'PSA'];
-candidatos['vereador'][6] = ['99111', 'Emerl', 'PSA'];
-candidatos['vereador'][7] = ['99854', 'Rouge', 'PSA'];
-candidatos['vereador'][8] = ['99765', 'Dr. Eggman', 'PSA'];
+candidatos['vereador'][6] = ['99555', 'Tails', 'PSA'];
+candidatos['vereador'][7] = ['99111', 'Emerl', 'PSA'];
+candidatos['vereador'][8] = ['99854', 'Rouge', 'PSA'];
+candidatos['vereador'][9] = ['99765', 'Dr. Eggman', 'PSA'];
 
-candidatos['vereador'][9] = ['77150', 'Mewtwo', 'PMB'];
-candidatos['vereador'][10] = ['77202', 'Wobbuffet', 'PMB'];
-candidatos['vereador'][11] = ['77249', 'Lugia', 'PMB'];
-candidatos['vereador'][12] = ['77250', 'Ho-oh', 'PMB'];
+candidatos['vereador'][10] = ['77150', 'Mewtwo', 'PMB'];
+candidatos['vereador'][11] = ['77202', 'Wobbuffet', 'PMB'];
+candidatos['vereador'][12] = ['77249', 'Lugia', 'PMB'];
+candidatos['vereador'][13] = ['77250', 'Ho-oh', 'PMB'];
 
 window.onload = iniciar;
 
