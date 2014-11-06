6990bc2a37b5ce86c0364c1aadd1d20a7a646ddd
Alisson Chiquitto <chiquitto@gmail.com>
2014-11-06 13:29:36 -0200

    3serie - Zend_Navigation

--- unipar-cianorte/tads/servicos-internet/zend-framework/sistema/private/application/Bootstrap.php
+++ unipar-cianorte/tads/servicos-internet/zend-framework/sistema/private/application/Bootstrap.php
@@ -8,5 +8,18 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view = $this->getResource('view');
         $view->doctype('HTML5');
     }
+    
+    protected function _initNavigation() {
+        $xml = APPLICATION_PATH
+                . '/configs/navigation.xml';
+        
+        $config = new Zend_Config_Xml($xml, 'nav');
+        $nav = new Zend_Navigation($config);
+        
+        $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
+        
+        $layout->getView()->navigation($nav);
+    }
 }
 
